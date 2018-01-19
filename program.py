499cd5f2892459ad6d7131192c41fe5505c2c913
braydensmith12 <31324932+braydensmith12@users.noreply.github.com>
2018-01-19 13:48:47 -0800

    Add files via upload

---
+++ program.py
@@ -0,0 +1,64 @@
+import PIL
+import matplotlib.pyplot as plt # single use of plt is commented out
+import os.path  
+import PIL.ImageDraw            
+
+def make_collage(original_image):
+    return original_image
+
+def get_images(directory=None):
+    """ Returns PIL.Image objects for all the images in directory.
+    
+    If directory is not specified, uses current directory.
+    Returns a 2-tuple containing 
+    a list with a  PIL.Image object for each image file in root_directory, and
+    a list with a string filename for each image file in root_directory
+    """
+    
+    if directory == None:
+        directory = os.getcwd() # Use working directory if unspecified
+        
+    image_list = [] # Initialize aggregaotrs
+    file_list = []
+    
+    directory_list = os.listdir(directory) # Get list of files
+    for entry in directory_list:
+        absolute_filename = os.path.join(directory, entry)
+        try:
+            image = PIL.Image.open(absolute_filename)
+            file_list += [entry]
+            image_list += [image]
+        except IOError:
+            pass # do nothing with errors tying to open non-images
+    return image_list, file_list
+
+def make_collages(number_of_images=1, border_shape='rectangle', filter_color='random', directory=None):
+    """ 
+    """
+    
+    if directory == None:
+        directory = os.getcwd() # Use working directory if unspecified
+        
+    # Create a new directory 'framed'
+    new_directory = os.path.join(directory, 'collages')
+    try:
+        os.mkdir(new_directory)
+    except OSError:
+        pass # if the directory already exists, proceed  
+    
+    # Load all the images
+    image_list, file_list = get_images(directory)  
+
+    # Go through the images and save modified versions
+    for n in range(len(image_list)):
+        # Parse the filename
+        print n
+        filename, filetype = os.path.splitext(file_list[n])
+        
+        # Round the corners with default percent of radius
+        curr_image = image_list[n]
+        new_image = make_collage(curr_image) 
+        
+        # Save the altered image, suing PNG to retain transparency
+        new_image_filename = os.path.join(new_directory, filename + '.png')
+        new_image.save(new_image_filename)
