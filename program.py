91bec0d05fce0722db8a674f040819fb7c945b96
Derek DMC <31111787+dmc573@users.noreply.github.com>
2018-01-22 13:52:38 -0800

    Add files via upload

--- program.py
+++ program.py
@@ -1,7 +1,8 @@
 import PIL
 import matplotlib.pyplot as plt # single use of plt is commented out
 import os.path  
-import PIL.ImageDraw            
+import PIL.ImageDraw
+import random
 
 def make_collage(original_image):
     return original_image
@@ -32,7 +33,7 @@ def get_images(directory=None):
             pass # do nothing with errors tying to open non-images
     return image_list, file_list
 
-def make_collages(number_of_images=1, border_shape='rectangle', filter_color='random', directory=None):
+def make_collages(number_of_images=2, border_shape='rectangle', filter_color='random', directory=None):
     """ 
     """
     
@@ -47,18 +48,42 @@ def make_collages(number_of_images=1, border_shape='rectangle', filter_color='ra
         pass # if the directory already exists, proceed  
     
     # Load all the images
-    image_list, file_list = get_images(directory)  
-
+    image_list, file_list = get_images(directory)
+    
+    random.shuffle(image_list)
+    
     # Go through the images and save modified versions
-    for n in range(len(image_list)):
-        # Parse the filename
-        print n
-        filename, filetype = os.path.splitext(file_list[n])
+    for n in range(len(image_list)/number_of_images):
         
         # Round the corners with default percent of radius
-        curr_image = image_list[n]
-        new_image = make_collage(curr_image) 
-        
+        curr_images = []
+        widths = []
+        heights = []
+        for i in range(number_of_images):
+            curr_images.append(image_list[2*n+i])
+            w, h = curr_images[i].size
+            widths.append(w)
+            heights.append(h)
+        if number_of_images == 2:
+            if sum(widths) > sum(heights): #vertical
+                width = min(widths)
+                if widths[0] == width:
+                    heights[1] = heights[1]*widths[0]/widths[1]
+                    widths[1] = widths[0]
+                else:
+                    heights[0] = heights[0]*widths[1]/widths[0]
+                    widths[0] = widths[1]
+                height = sum(heights)
+                curr_images[0] = curr_images[0].resize((widths[0],heights[0]))
+                curr_images[1] = curr_images[1].resize((widths[1],heights[1]))
+                canvas = PIL.Image.new('RGB', (width, height), 'white')
+                canvas.paste(curr_images[0], (0,0))
+                canvas.paste(curr_images[1], (0, heights[0]))
+                new_image_filename = os.path.join(new_directory, 'collage' + str(n+1) + '.png')
+                canvas.save(new_image_filename)
+                print n
+            else: #horizontal
+                pass
+            
         # Save the altered image, suing PNG to retain transparency
-        new_image_filename = os.path.join(new_directory, filename + '.png')
-        new_image.save(new_image_filename)
+        
